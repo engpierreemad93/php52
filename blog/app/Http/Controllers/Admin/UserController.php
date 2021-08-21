2d838bcdafcba5d17f280571e153fad5afd4cbe0
engpierreemad93 <pierreemad93@gmail.com>
2021-08-21 19:32:19 +0200

    init app

---
+++ blog/app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $users = User::all();
+        // return $users ;
+        return view('admin.user.all' , compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('admin.user.create');
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // 
+        $data = $request->all(); 
+        $rules = [
+            'name' => ['required' ,'min:4' , 'max:25'] , 
+            'email' => ['required' , 'unique:users'] , 
+            'password' =>['required'] , 
+        ]; 
+        $validator =Validator::make($data , $rules) ;
+        if($validator->fails()){
+            return redirect()->back()->withErrors($validator)->withInput($data);
+        }
+        User::create([
+            'name' => $request->name ,
+            'email' => $request->email , 
+            'password' => Hash::make($request->password) ,
+
+        ]);
+        return redirect()->back();
+        // return redirect()->route('user.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ blog/resources/views/admin/user/all.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.admin')
+@section('title') users @endsection
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header card-header-primary">
+                <h4 class="card-title ">Simple Table</h4>
+                <p class="card-category"> Here is a subtitle for this table</p>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead class=" text-primary">
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>email</th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($users as $user)
+                            <tr>
+                                <td></td>
+                                <td>{{$user->name }}</td>
+                                <td>{{$user->email }}</td>
+                                <td></td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                <a href="user/create">add user</a>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
